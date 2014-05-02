00e1e1f91851d48dedbaf9027fd4555785293b69
Rubén Agudo <rubenagudo21@hotmail.com>
2014-05-02 17:19:11 +0200

    Añadido el php que funciona

--- Labo9/Labo9-php/login.php
+++ Labo9/Labo9-php/login.php
@@ -1,46 +1,37 @@
-
 <?php
+echo 'hola php';
+$parametro = $_GET['parametro'];
+switch ($parametro) {
+    case '0':
+        $usuarios = getUsers();
+        echo 'el telefono es: '.$usuarios[0];
+        break;
 
-$elegirFuncion = $_GET['funcion'];
-switch($elegirFuncion) {
-    case 0:
-        getUsers();
-        break;
-    case 1:
-        break;
-    case 2:
-        break;
     default:
         echo 0;
+        break;
 }
 
 function conectaDB() {
-	$hostname = 'localhost';
-	$username = 'labo9';
-	$password = '1234';
-    $dbname = 'labo9';
-	try {
-		$db = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-		$db->exec("set names utf8");
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		return($db);
-	} catch (PDOException $e) {
-		echo $e->getMessage();
-		exit();
-	}
-}
-
-function getUsers() {
-    
-
-	$dbh = conectaDB();
-	$consult_families = $dbh->prepare("SELECT * FROM Usuarios"); 
-	$consult_families->execute();
-	$data_families = $consult_families->fetchAll();
-    echo $data_families['TlfnoUsuario'];
-    $test = [1,2,3,4,5,6];
-
-    foreach($num as $test) {
-        echo $num;
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '1234';
+    try {
+        $db = new PDO("mysql:host=$hostname;dbname=LABORATORIO9", $username, $password);
+        $db->exec("set names utf8");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return($db);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit();
     }
 }
+function getUsers() {
+    $dbh = conectaDB();
+    $consult_usuarios = $dbh->prepare("SELECT * FROM usuarios");
+    $consult_usuarios->execute();
+    $data_usuarios = $consult_usuarios->fetch();
+    if (!empty($data_usuarios)) return $data_usuarios;
+    else return 0;
+}
+?> 
