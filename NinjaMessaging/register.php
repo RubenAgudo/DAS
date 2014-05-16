0c2f070c670b9d2172c4be77f86fe805d9c3f4a0
RubenAgudo <rubenagudo21@gmail.com>
2014-05-16 15:51:21 +0200

    Añadido el php modificado

--- NinjaMessaging/register.php
+++ NinjaMessaging/register.php
@@ -4,7 +4,7 @@ $operacion =$_POST['codigo'];
 
 switch($operacion) {
     case 0:
-        echo checkIfExistsRegistration($_POST['telefono']);
+        echo getUsers();
         break;
     case 1:
         $imei = $_POST['IMEI'];
@@ -26,7 +26,20 @@ switch($operacion) {
             'data' => $data); 
         peticionCurl($info, $cabecera);
         break;
-     
+    case 3:
+        $cabecera = array(
+            'Authorization: key=AIzaSyDfgT87fvHf9E2Ad_wQsLEmjEkdafiHPvY',
+            'Content-type: Application/json');
+        $data = array('latitude' => $_POST['latitude'],
+            'longitude' => $_POST['longitude'],
+            'isMap' => true);
+        $regid = obtenerRegidDesdeIMEI($_POST['toUser']);
+        $info = array(
+            'registration_ids' => array($regid),
+            'data' => $data);
+        peticionCurl($info, $cabecera);
+        break;
+
 }
 
 function peticionCurl($info, $cabecera) {
@@ -49,6 +62,18 @@ function peticionCurl($info, $cabecera) {
 
     #cerrar el handler de curl
     curl_close($ch);
+}
+
+function getUsers() {
+    $dbh = conectaDB();
+	$consult_registro= $dbh->prepare("SELECT usuario FROM registro");
+	$consult_registro->execute();
+	$data_proximas = $consult_registro->fetchAll();
+	if (!empty($data_proximas)) {
+		return json_encode($data_proximas);
+	} else {
+		return null;
+	}
 }
 
 function obtenerRegidDesdeIMEI($toUser) {
